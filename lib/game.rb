d45c8de2a9c60f59a69166d963c050d046431e5a
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 19:47:18 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -20,7 +20,7 @@ class Game
 
   #state remembered interactions
   def current_player(board= @board)
-    @board.turn_count.even? @player_1 : @player_2
+    @board.turn_count %2 == 0? @player_1 : @player_2
   end
 
   def won?
